b4bdc533e2a1503207344f5a7c5fc9c75810f516
ranyuan <ranyuan@foxmail.com>
2016-01-22 01:44:09 +0800

    首次提交

---
+++ src/www/beifeng/hivef/Main.java
@@ -0,0 +1,16 @@
+package www.beifeng.hivef;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	
+	{
+        
+		System.out.println(args[0]);
+
+	}
+
+}
